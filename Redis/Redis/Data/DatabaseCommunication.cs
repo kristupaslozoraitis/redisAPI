6053a1d3e755e66935a4aeae3a8c0c999d7f1ec1
kristupaslozoraitis <65513238+kristupaslozoraitis@users.noreply.github.com>
2022-11-21 19:35:42 +0200

    Implemented sync between horizontal fragments

--- Redis/Redis/Data/DatabaseCommunication.cs
+++ Redis/Redis/Data/DatabaseCommunication.cs
@@ -18,10 +18,10 @@ namespace Redis.Data
         public DatabaseCommunication(IConnectionMultiplexer redis)
         {
             _redis = redis;
-            db_ProdArchivedYears = _redis.GetDatabase(0);
-            db_ProdCurrentYears = _redis.GetDatabase(1);
-            db_SalesArchivedYears = _redis.GetDatabase(2);
-            db_SalesCurrentYears = _redis.GetDatabase(3);
+            db_ProdArchivedYears = _redis.GetDatabase(3);
+            db_ProdCurrentYears = _redis.GetDatabase(2);
+            db_SalesArchivedYears = _redis.GetDatabase(1);
+            db_SalesCurrentYears = _redis.GetDatabase(0);
         }
 
         public void DeleteData(string key, string db)
@@ -115,6 +115,42 @@ namespace Redis.Data
                     break;
                 default:
                     return;
+            }
+
+            if(values.Any((value) => value.Name == "order_date" && value.Value.ToString().Substring(0, 4) == "2022"))
+            {
+                var keyT = "sales.order_items:" + key.Substring(13) + ":*";
+                var orderItems = _redis.GetServer("127.0.0.1:6379").Keys(1, keyT);
+                foreach (var item in orderItems)
+                {
+                    var t = db_SalesArchivedYears.HashGetAll(item);
+                    db_SalesCurrentYears.HashSet(item, t);
+                    db_SalesArchivedYears.KeyDelete(item);
+                }
+                var exist = db_SalesArchivedYears.HashGetAll(key);
+                if(exist.Length > 0)
+                {
+                    db_SalesArchivedYears.KeyDelete(key);
+                }
+                db_SalesCurrentYears.HashSet(key, values.ToArray());
+                return;
+            } else if(values.Any((value) => value.Name == "order_date" && value.Value.ToString().Substring(0, 4) != "2022"))
+            {
+                var keyT = "sales.order_items:" + key.Substring(13) + ":*";
+                var orderItems = _redis.GetServer("127.0.0.1:6379").Keys(0, keyT);
+                foreach (var item in orderItems)
+                {
+                    var t = db_SalesCurrentYears.HashGetAll(item);
+                    db_SalesArchivedYears.HashSet(item, t);
+                    db_SalesCurrentYears.KeyDelete(item);
+                }
+                var exist = db_SalesCurrentYears.HashGetAll(key);
+                if (exist.Length > 0)
+                {
+                    db_SalesCurrentYears.KeyDelete(key);
+                }
+                db_SalesArchivedYears.HashSet(key, values.ToArray());
+                return;
             }
 
             db.HashSet(key, values.ToArray());
